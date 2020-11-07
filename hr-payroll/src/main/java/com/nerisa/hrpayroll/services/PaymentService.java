bbaf800c00da0695487ded73aec958de353cbbc8
Alessandro Neri <alessandroneri@gmail.com>
2020-11-06 23:42:07 -0300

    Add Feign Client.

--- hr-payroll/src/main/java/com/nerisa/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/nerisa/hrpayroll/services/PaymentService.java
@@ -2,6 +2,7 @@ package com.nerisa.hrpayroll.services;
 
 import com.nerisa.hrpayroll.entities.Payment;
 import com.nerisa.hrpayroll.entities.Worker;
+import com.nerisa.hrpayroll.feignclients.WorkerFeignClients;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,15 +14,11 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
-    private final RestTemplate restTemplate;
+    private final WorkerFeignClients workerFeignClients;
 
     public Payment getPayment(long workerId, int days){
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", String.valueOf(workerId));
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+
+        Worker worker = workerFeignClients.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
