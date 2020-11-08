c34946c0b522441ca20fdee9f3f901a1ad93124c
Alessandro Neri <alessandroneri@gmail.com>
2020-11-08 12:54:12 -0300

    Add Load Balancing

--- hr-payroll/src/main/java/com/nerisa/hrpayroll/feignclients/WorkerFeignClients.java
+++ hr-payroll/src/main/java/com/nerisa/hrpayroll/feignclients/WorkerFeignClients.java
@@ -1,15 +1,15 @@
 package com.nerisa.hrpayroll.feignclients;
 
 import com.nerisa.hrpayroll.entities.Worker;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import java.util.List;
 
 @Component
-@FeignClient(name = "hr-worker", url="localhost:8001", path="/workers")
+@FeignClient(name = "hr-worker", path = "/workers")
 public interface WorkerFeignClients {
 
     @GetMapping(value = "/{id}")
