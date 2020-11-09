df7f4000416b924d88cee89d5483394bc58799a5
Alessandro Neri <alessandroneri@gmail.com>
2020-11-09 17:16:59 -0300

    hr-worker as a config server cliente.

--- hr-worker/src/main/java/com/nerisa/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/nerisa/hrworker/resources/WorkerResource.java
@@ -4,6 +4,8 @@ import com.nerisa.hrworker.entities.Worker;
 import com.nerisa.hrworker.repositories.WorkerRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,13 +17,23 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value="/workers")
-@RequiredArgsConstructor
 @Slf4j
 public class WorkerResource {
 
-    private final Environment environment;
+    @Value("${test.config}")
+    private String testConfig;
 
-    private final WorkerRepository workerRepository;
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private WorkerRepository workerRepository;
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs() {
+        log.info("CONFIG = " + testConfig);
+        return ResponseEntity.noContent().build();
+    }
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
