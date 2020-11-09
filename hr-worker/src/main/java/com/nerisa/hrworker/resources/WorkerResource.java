7e528ba3803637bd01f52065643038f702d3f013
Alessandro Neri <alessandroneri@gmail.com>
2020-11-09 17:33:59 -0300

    Actuator for runtime config request

--- hr-worker/src/main/java/com/nerisa/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/nerisa/hrworker/resources/WorkerResource.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
+@RefreshScope
 @RestController
 @RequestMapping(value="/workers")
 @Slf4j
