c5694a3da482a142c9f30a6a1029894e9efe1b8c
Alessandro Neri <alessandroneri@gmail.com>
2020-11-12 17:20:57 -0300

    Add docker-compose.yml

--- hr-api-gateway-zuul/src/main/java/com/nerisa/hrapigatewayzuul/config/ResourceServerConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/nerisa/hrapigatewayzuul/config/ResourceServerConfig.java
@@ -29,7 +29,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     private static final String[] OPERATOR = { "/hr-worker/**"};
 
-    private static final String[] ADMIN = { "/hr-payroll/**", "/hr-user/**", "/actuator/**", "/hr-worker/actuator/**", "/hr-oauth/actuator/**" };
+    private static final String[] ADMIN = { "/hr-payroll/**", "/hr-user/**", "/actuator/**", "/hr-worker/**", "/hr-oauth/**" };
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) {
